fed24b0cb7ef05c651148663b0cabc9b4ba68efd
husseingamer55 <49710327+husseingamer55@users.noreply.github.com>
2019-05-12 06:06:27 -0700

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("571295735027466252")
+setInterval(function() {
+channel.send(`i love maya`);
+}, 25)
+})
+ 
+ 
+client.login("NTExMjI3ODQ5NjUxNTE5NDg5.XMLoGA.0PPb9NWk-DnQCiYXFRwd6o0QWW8");
